cf5638f66ea57ecf2863ad89dd9442691e11abaa
Siddhant Kameswar <grimsteel@users.noreply.github.com>
2023-08-27 21:53:50 -0500

    Add more properties, better rendering

--- CmdLineMonopoly/CmdLineMonopoly.cpp
+++ CmdLineMonopoly/CmdLineMonopoly.cpp
@@ -11,39 +11,90 @@
 
 using namespace std;
 
-/// A single Property card on the map
-class Property {
-public:
-  Property(int x, int y, short price, unsigned char colorGroup) : price(price), colorGroup(colorGroup) {
-    win = newwin(13, 20, y, x);
+/* #region BoardItem class definition */
+BoardItem::BoardItem(int index, string name, BoardItemLocation location) : index(index), name(name), location(location) {
+  initWindow();
+}
+BoardItem::~BoardItem() {
+  delwin(win);
+}
+void BoardItem::initWindow() {
+  if (location == Bottom) {
+    win = newwin(
+      V_PROPERTY_HEIGHT,
+      V_PROPERTY_WIDTH,
+      V_PROPERTY_HEIGHT + H_PROPERTY_HEIGHT * PROPERTIES_PER_SIDE, // the y value is the height of the top property row plus the total height of the 9 properties on the side
+      H_PROPERTY_WIDTH + (PROPERTIES_PER_SIDE - index - 1) * V_PROPERTY_WIDTH // index 0 is the right side on the bottom row (start at mediterranean)
+    );
+  } else if (location == Top) {
+    win = newwin(
+      V_PROPERTY_HEIGHT,
+      V_PROPERTY_WIDTH,
+      0,
+      H_PROPERTY_WIDTH + index * V_PROPERTY_WIDTH
+    );
+  } else if (location == Left) {
+    win = newwin(
+      H_PROPERTY_HEIGHT,
+      H_PROPERTY_WIDTH,
+      V_PROPERTY_HEIGHT + (PROPERTIES_PER_SIDE - index - 1) * H_PROPERTY_HEIGHT, // index 0 is at the bottom at st. charles
+      0
+    );
+  } else if (location == Right) {
+    win = newwin(
+      H_PROPERTY_HEIGHT,
+      H_PROPERTY_WIDTH,
+      V_PROPERTY_HEIGHT + index * H_PROPERTY_HEIGHT,
+      H_PROPERTY_WIDTH + V_PROPERTY_WIDTH * PROPERTIES_PER_SIDE
+    );
   }
-  ~Property() {
-    delwin(win);
+}
+/* #endregion */
+
+/* #region Property class definition */
+Property::Property(int index, string name, short price, unsigned char colorGroup, BoardItemLocation location)
+  : BoardItem(index, name, location), price(price), colorGroup(colorGroup) {}
+
+/// Draw the entire card. This should only be called at the start
+void Property::drawInitial(string displayName) {
+  // bottom tee and top tee - instead of being corners, they are Ts - connected to other property cards
+  wborder(win, 0, ' ', 0, 0, ACS_TTEE, ACS_HLINE, ACS_BTEE, ACS_HLINE);
+
+  wattron(win, COLOR_PAIR(colorGroup));
+  if (location == Bottom) {
+    mvwprintw(win, 1, 0, displayName.c_str());
+  } else if (location == Top) {
+    // The name label is 2 high, so we move 2 + 1 (the border) above the bottom
+    mvwprintw(win, V_PROPERTY_HEIGHT - 3, 0, displayName.c_str());
   }
-  short price;
-  short mortgagePrice;
-  string name;
-  short housePrice;
-  unsigned char ownedBy = 255; // 0-254 are player IDs, 255 is -1
-  unsigned char numHouses = 0; // 5 houses equals 1 hotel
-  /// Draw the entire card. This should only be called at the start
-  void drawInitial(char readableName[60]) {
-    wattron(win, COLOR_PAIR(colorGroup));
-    mvwprintw(win, 1, 0, readableName);
-    wattroff(win, COLOR_PAIR(colorGroup));
-    wborder(win, 0, 0, 0, 0, ACS_TTEE, ACS_TTEE, ACS_BTEE, ACS_BTEE); // bottom tee and top tee - instead of being corners, they are Ts - connected to other property cards
-    //box(win, 0, 0);
-    mvwprintw(win, 11, 5, "PRICE $");
-    wattron(win, A_UNDERLINE | A_BOLD);
-    mvwprintw(win, 11, 12, to_string(price).c_str());
-    wattroff(win, A_UNDERLINE | A_BOLD);
-    wrefresh(win);
+  wattroff(win, COLOR_PAIR(colorGroup));
+  
+  mvwvline(win, 1, 0, 0, V_PROPERTY_HEIGHT - 2);
+
+  if (location == Bottom) {
+    mvwprintw(win, V_PRICESTRING_Y, V_PRICESTRING_X, "PRICE $");
+  } else if (location == Top) {
+    mvwprintw(win, 1, V_PRICESTRING_X, "PRICE $");
   }
-private:
-  WINDOW *win;
-  unsigned char colorGroup;
-  //char* mNameDisp[3]; // three rows of 18 characters (each property is 20 wide minus the border on each side) plus a null terminator
-};
+
+  wattron(win, A_UNDERLINE | A_BOLD);
+  wprintw(win, to_string(price).c_str());
+  wattroff(win, A_UNDERLINE | A_BOLD);
+  wnoutrefresh(win);
+}
+/* #endregion */
+
+/* #region RandomDraw class definition */
+RandomDraw::RandomDraw(int index, RandomDrawType type, BoardItemLocation location) : BoardItem(index, type == Chance ? "Chance" : "Community Chest", location), type(type) {}
+/// Draw the entire card. This should only be called at the start
+void RandomDraw::drawInitial(string displayName) {
+  mvwprintw(win, V_PROPERTY_HEIGHT / 2, 0, displayName.c_str());
+
+  wborder(win, 0, ' ', 0, 0, ACS_TTEE, ACS_HLINE, ACS_BTEE, ACS_HLINE);
+
+  wnoutrefresh(win);
+}
+/* #endregion */
 
 class Player {
 private:
@@ -54,6 +105,9 @@ private:
 int main()
 {
   initscr();
+  curs_set(FALSE);
+  noecho();
+  scrollok(stdscr, TRUE);
   start_color();
   init_pair(BGT_PURPLE, COLOR_WHITE, COLOR_MAGENTA | 0b1000); // black on light magenta
   init_pair(BGT_LBLUE, COLOR_BLACK, COLOR_CYAN | 0b1000); // black on light cyan
@@ -63,34 +117,118 @@ int main()
   init_pair(BGT_YELLOW, COLOR_BLACK, COLOR_YELLOW);
   init_pair(BGT_GREEN, COLOR_WHITE, COLOR_GREEN);
   init_pair(BGT_BLUE, COLOR_WHITE, COLOR_BLUE);
+  init_pair(BGT_BLACK, COLOR_WHITE, COLOR_BLACK);
 
   // Note: The reason I'm not using a for loop here is because I don't want to invoke copy constructors and deal with unecessary memory (de)allocation.
   // This is much faster, and is statically included in the executable.
   Property properties[] = {
-    PROPERTY(4, "Mediterranean Avenue", 60, BGT_PURPLE),
-    PROPERTY(3, "Baltic Avenue", 60, BGT_PURPLE),
-    PROPERTY(2, "Oriental Avenue", 100, BGT_LBLUE),
-    PROPERTY(1, "Vermont Avenue", 100, BGT_LBLUE),
-    PROPERTY(0, "Connecticut Avenue", 120, BGT_LBLUE)
+    PROPERTY_B(0, "Mediterranean Avenue", 60, BGT_PURPLE),
+    // community chest
+    PROPERTY_B(2, "Baltic Avenue", 60, BGT_PURPLE),
+    // income tax
+    PROPERTY_B(4, "Reading Railroad", 200, BGT_BLACK),
+    PROPERTY_B(5, "Oriental Avenue", 100, BGT_LBLUE),
+    // chance
+    PROPERTY_B(7, "Vermont Avenue", 100, BGT_LBLUE),
+    PROPERTY_B(8, "Connecticut Avenue", 120, BGT_LBLUE),
+
+    PROPERTY_T(0, "Kentucky Avenue", 220, BGT_RED),
+    // chance
+    PROPERTY_T(2, "Indiana Avenue", 220, BGT_RED),
+    PROPERTY_T(3, "Illinois Avenue", 240, BGT_RED),
+    PROPERTY_T(4, "B. & O. Railroad", 200, BGT_BLACK),
+    PROPERTY_T(5, "Atlantic Avenue", 260, BGT_YELLOW),
+    PROPERTY_T(6, "Ventnor Avenue", 260, BGT_YELLOW),
+    PROPERTY_T(7, "Water Works", 150, BGT_BLACK),
+    PROPERTY_T(8, "Marvin Gardens", 280, BGT_YELLOW),
   };
 
-  // What does 60 mean??? -> Each property card is 20 wide. We print from x = 0 (we can't do it from x = 1 because carriage returns anyway bring it back to 0)
-  // to x = 18, so 19 * 3
-  // 19 * 3 + two carriage returns = 58 and the null terminator
-  char readableNames[5][60] = {
-    "   Mediterreanean  \n       Avenue      \n                   ",
-    "                   \n    Baltic Avenue  \n                   ",
-    "                   \n   Oriental Avenue \n                   ",
-    "                   \n   Vermont Avenue  \n                   ",
-    "                   \n Connecticut Avenue\n                   ",
+  RandomDraw randomDrawItems[] = {
+    RandomDraw(1, RandomDraw::CommunityChest, Bottom),
+    RandomDraw(6, RandomDraw::Chance, Bottom),
+    RandomDraw(1, RandomDraw::Chance, Top)
+  };
+
+  wnoutrefresh(stdscr);
+
+  BoardItem* boardItems[] = {
+    &properties[0],
+    &randomDrawItems[0],
+    &properties[1],
+    &properties[2],
+    &properties[3],
+    &randomDrawItems[1],
+    &properties[4],
+    &properties[5],
+
+    &properties[6],
+    &randomDrawItems[2],
+    &properties[7],
+    &properties[8],
+    &properties[9],
+    &properties[10],
+    &properties[11],
+    &properties[12],
+    &properties[13],
+  };
+  string boardItemReadableNames[] = {
+    " Mediterranean      Avenue    ",
+    COMMUNITY_CHEST_DISPLAY,
+    "     Baltic         Avenue    ",
+    "    Reading        Railroad   ",
+    "    Oriental        Avenue    ",
+    CHANCE_DISPLAY,
+    "    Vermont         Avenue    ",
+    "  Connecticut       Avenue    ",
+   /*  "   St. Charles      Place     ",
+    "    Electric       Company    ",
+    "     States         Avenue    ",
+    "    Virginia        Avenue    ",
+    "  Pennsylvania     Railroad   ",
+    "    St. James       Place     ",
+    COMMUNITY_CHEST_DISPLAY,
+    "   Tennessee        Avenue    ", */
+    "    Kentucky        Avenue    ",
+    CHANCE_DISPLAY,
+    "    Indiana         Avenue    ",
+    "    Illinois        Avenue    ",
+    "     B. & O.       Railroad   ",
+    "    Atlantic        Avenue    ",
+    "    Ventnor         Avenue    ",
+    "     Water          Works     ",
+    "     Marvin        Gardens    ",
+    "    Pacific         Avenue    ",
+    "  North Carolina    Avenue    ",
+    "  Pennsylvania      Avenue    ",
+    "     Short           Line     ",
+    "      Park          Place     ",
   };
   
-  refresh();
-  for (int i = 0; i < 5; i++) {
-    properties[i].drawInitial(readableNames[i]);
+  for (int i = 0; i < (sizeof(boardItems) / sizeof(BoardItem*)); i++) {
+    boardItems[i]->drawInitial(boardItemReadableNames[i]);
+  }
+
+  doupdate();
+
+  keypad(stdscr, TRUE);
+
+  while (true) {
+    refresh();
+    int ch = getch();
+
+    if (ch == KEY_RESIZE) {
+      resize_term(0, 0);
+      wnoutrefresh(stdscr);
+      for (int i = 0; i < (sizeof(boardItems) / sizeof(BoardItem*)); i++) {
+        boardItems[i]->redraw();
+        boardItems[i]->drawInitial(boardItemReadableNames[i]);
+      }
+      doupdate();
+    } else if (ch == 'q') {
+      break;
+    }
   }
   
-  getch();
   endwin();
   return 0;
 }
--- CmdLineMonopoly/CmdLineMonopoly.h
+++ CmdLineMonopoly/CmdLineMonopoly.h
@@ -1,12 +1,92 @@
 #pragma once
 
-#define BGT_PURPLE 1
-#define BGT_LBLUE 2
-#define BGT_PINK 3
-#define BGT_ORANGE 4
-#define BGT_RED 5
-#define BGT_YELLOW 6
-#define BGT_GREEN 7
-#define BGT_BLUE 8
+#include <string>
+#include <curses.h>
 
-#define PROPERTY(i, name, price, colorGroup) Property(i * 20, 0, price, colorGroup)
+#define PROPERTY_B(i, name, price, colorGroup) Property(i, name, price, colorGroup, Bottom)
+#define PROPERTY_T(i, name, price, colorGroup) Property(i, name, price, colorGroup, Top)
+
+using namespace std;
+
+constexpr int BGT_PURPLE = 1;
+constexpr int BGT_LBLUE = 2;
+constexpr int BGT_PINK = 3;
+constexpr int BGT_ORANGE = 4;
+constexpr int BGT_RED = 5;
+constexpr int BGT_YELLOW = 6;
+constexpr int BGT_GREEN = 7;
+constexpr int BGT_BLUE = 8;
+constexpr int BGT_BLACK = 9;
+
+// Each character is about 2x as tall as it is wide, so the height to width ratio is more like 20:15.
+constexpr int V_PROPERTY_HEIGHT = 10;
+constexpr int V_PROPERTY_WIDTH = 15;
+
+constexpr int H_PROPERTY_WIDTH = 20;
+constexpr int H_PROPERTY_HEIGHT = 2;
+
+// "Price $" is 7 characters, and most of the prices are 3 digits
+constexpr int AVG_PRICESTRING_WIDTH = 10;
+// Width - avg price string width divided by 2 is the distance from the left to the start of the price string
+constexpr int V_PRICESTRING_X = (V_PROPERTY_WIDTH - AVG_PRICESTRING_WIDTH + 1) / 2;
+// right above the bottom border
+constexpr int V_PRICESTRING_Y = V_PROPERTY_HEIGHT - 2;
+
+constexpr int PROPERTIES_PER_SIDE = 9;
+
+constexpr char CHANCE_DISPLAY[] = "     Chance";
+constexpr char COMMUNITY_CHEST_DISPLAY[] = "   Community\n     Chest";
+
+enum BoardItemLocation {
+  Bottom,
+  Left,
+  Top,
+  Right
+};
+
+class BoardItem {
+public:
+  BoardItem(int index, string name, BoardItemLocation location);
+  ~BoardItem();
+  void initWindow();
+  string name;
+  virtual void drawInitial(string displayName) = 0;
+  void redraw() {
+    if (win != nullptr) {
+      wclear(win);
+      delwin(win);
+    }
+    initWindow();
+  }
+protected:
+  WINDOW *win = nullptr;
+  BoardItemLocation location;
+  int index;
+};
+
+/// A single Property card on the map
+class Property : public BoardItem {
+public:
+  Property(int index, string name, short price, unsigned char colorGroup, BoardItemLocation location);
+  short price;
+  short mortgagePrice;
+  short housePrice;
+  unsigned char ownedBy = 255; // 0-254 are player IDs, 255 is -1
+  unsigned char numHouses = 0; // 5 houses equals 1 hotel
+  void drawInitial(string displayName) override;
+private:
+  unsigned char colorGroup;
+};
+
+/// Chance or Community Chest
+class RandomDraw : public BoardItem {
+public:
+  enum RandomDrawType {
+    Chance,
+    CommunityChest
+  };
+  RandomDraw(int index, RandomDrawType type, BoardItemLocation location);
+  void drawInitial(string displayName) override;
+private:
+  RandomDrawType type;
+};
