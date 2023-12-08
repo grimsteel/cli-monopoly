8c4b9a82c2e40cdb95341dc2fe821a0a28a69013
Siddhant Kameswar <grimsteel@users.noreply.github.com>
2023-12-08 16:17:59 -0600

    Start working on board center

--- CmdLineMonopoly/BoardCenter.cpp
+++ CmdLineMonopoly/BoardCenter.cpp
@@ -6,35 +6,56 @@
 #include <curses.h>
 #include <thread>
 
+using namespace std;
+
 constexpr int BOARD_CENTER_X = H_PROPERTY_WIDTH + PROPERTIES_PER_SIDE * V_PROPERTY_WIDTH / 2;
 constexpr int BOARD_CENTER_Y = V_PROPERTY_HEIGHT + PROPERTIES_PER_SIDE * H_PROPERTY_HEIGHT / 2;
-constexpr int BOARD_CENTER_WIDTH = 11;
-constexpr int BOARD_CENTER_HEIGHT = 3;
+constexpr int BOARD_CENTER_WIDTH = 20;
+constexpr int BOARD_CENTER_HEIGHT = 5;
+constexpr int DICE_WIDTH = 11;
+constexpr int DICE_HEIGHT = 3;
 
 BoardCenter::BoardCenter() {
   win = newwin(BOARD_CENTER_HEIGHT, BOARD_CENTER_WIDTH, BOARD_CENTER_Y - BOARD_CENTER_HEIGHT / 2, BOARD_CENTER_X - BOARD_CENTER_WIDTH / 2);
+  diceWin = derwin(win, DICE_HEIGHT, DICE_WIDTH, (BOARD_CENTER_HEIGHT - DICE_HEIGHT) / 2, (BOARD_CENTER_WIDTH - DICE_WIDTH) / 2);
 }
 
 BoardCenter::~BoardCenter() {
   delwin(win);
+  delwin(diceWin);
 }
 
 void BoardCenter::showDiceRoll(unsigned char roll1, unsigned char roll2) {
   wclear(win);
-  wattron(win, COLOR_PAIR(BGT_WHITE));
-  mvwaddwstr(win, 0, 0, L"┌───┐ ┌───┐│   │ │   │└───┘ └───┘");
-  wattroff(win, COLOR_PAIR(BGT_WHITE));
+  wclear(diceWin);
+  wattron(diceWin, COLOR_PAIR(BGT_WHITE));
+  mvwaddwstr(diceWin, 0, 0, L"┌───┐ ┌───┐\n│   │ │   │\n└───┘ └───┘");
+  wattroff(diceWin, COLOR_PAIR(BGT_WHITE));
   // clear the characters between the two dice
-  mvwvline(win, 0, BOARD_CENTER_WIDTH / 2, ' ', BOARD_CENTER_HEIGHT);
-  wattron(win, COLOR_PAIR(BGT_WHITE));
+  mvwvline(diceWin, 0, DICE_WIDTH / 2, ' ', DICE_HEIGHT);
+  wattron(diceWin, COLOR_PAIR(BGT_WHITE));
   for (char currentDigit = '1'; currentDigit <= '6'; currentDigit++) {
-    mvwaddch(win, 1, 2, currentDigit);
-    mvwaddch(win, 1, 8, '6' - currentDigit + '1');
-    wrefresh(win);
+    mvwaddch(diceWin, 1, 2, currentDigit);
+    mvwaddch(diceWin, 1, 8, '6' - currentDigit + '1');
+
+    wnoutrefresh(diceWin);
+    doupdate();
+
     std::this_thread::sleep_for(100ms);
   }
-  mvwaddch(win, 1, 2, roll1 + '0');
-  mvwaddch(win, 1, 8, roll2 + '0');
-  wattroff(win, COLOR_PAIR(BGT_WHITE));
-  wrefresh(win);
+  mvwaddch(diceWin, 1, 2, roll1 + '0');
+  mvwaddch(diceWin, 1, 8, roll2 + '0');
+  wattroff(diceWin, COLOR_PAIR(BGT_WHITE));
+
+  wnoutrefresh(diceWin);
+  doupdate();
+}
+
+void BoardCenter::showChanceDraw(string text, RandomDraw::RandomDrawType type) {
+  chtype bgStyle = COLOR_PAIR(type == RandomDraw::RandomDrawType::Chance ? BGT_ORANGE : BGT_YELLOW);
+  wclear(win);
+  wclear(diceWin);
+  wattron(win, bgStyle);
+  mvwaddwstr(win, 0, 0, L"┌─────────────────┐│                 ││                 ││                 │└─────────────────┘");
+  wattroff(win, bgStyle);
 }
--- CmdLineMonopoly/BoardCenter.h
+++ CmdLineMonopoly/BoardCenter.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <curses.h>
+#include "BoardItems.h"
 
 using namespace std;
 
@@ -12,7 +13,8 @@ public:
   ~BoardCenter();
   void showLogo();
   void showDiceRoll(unsigned char roll1, unsigned char roll2);
-  void showChanceDraw(string text);
+  void showChanceDraw(string text, RandomDraw::RandomDrawType type);
 private:
   WINDOW* win;
+  WINDOW* diceWin;
 };
